1c9a4c16b8500cc186957b88b0c98f22456c5eca
Oatymeany <ueaangkun.inyaem@gmail.com>
2023-09-16 12:09:25 +0700

    test: add more test item

--- Lab6_StrategyPattern/src/task2/App.java
+++ Lab6_StrategyPattern/src/task2/App.java
@@ -20,6 +20,28 @@ public class App {
         cart.setDiscountStrategy(new FixedDiscountStrategy(5.99));
         System.out.println("FixedDiscountStrategy");
         cart.calculateTotal();
+
+        Item item3 = new Item("iPhone 15", 2, 999.99);
+        Item item4 = new Item("iPhone 15 plus", 1, 1099.99);
+        Item item5 = new Item("iPhone 15 pro", 3, 1199.99);
+
+        for (int i = 0; i < 3; i++) {
+            cart.addItem(item3);
+            cart.addItem(item4);
+            cart.addItem(item5);
+        }
+
+        System.out.println();
+        System.out.println("After adding 3 more items");
+
+        cart.setDiscountStrategy(new PercentageDiscountStrategy(5));
+        System.out.println("PercentageDiscountStrategy");
+        cart.calculateTotal();
+        System.out.println();
+
+        cart.setDiscountStrategy(new FixedDiscountStrategy(200));
+        System.out.println("FixedDiscountStrategy");
+        cart.calculateTotal();
     }
     
 }
